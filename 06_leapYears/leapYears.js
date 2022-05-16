9290ee35a43757aa1ef3fd1e4b8f31bfe25575e2
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-16 15:37:18 -0500

    Update based on solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,17 +1,21 @@
 const leapYears = function(year) {
-    if (year % 4 === 0) {
-        if (year % 400 === 0 && year % 100 === 0) {
-            return true;
-        } else if (year % 100 === 0) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-    
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
+
+// const leapYears = function(year) {
+    // if (year % 4 === 0) {
+        // if (year % 400 === 0 && year % 100 === 0) {
+            // return true;
+        // } else if (year % 100 === 0) {
+            // return false;
+        // } else {
+            // return true;
+        // }
+    // } else {
+        // return false;
+    // }
+    // 
+// };
 
 // Do not edit below this line
 module.exports = leapYears;

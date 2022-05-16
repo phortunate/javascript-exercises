f5cf67e4171ca227845a4340ef0eced651d9c276
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-16 14:52:01 -0500

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let startNumber = num1;
+    let endNumber = num2;
+    let sum = 0;
 
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        startNumber = num2;
+        endNumber = num1;
+    }
+    
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
-
+sumAll("test", "e", 4)
 // Do not edit below this line
 module.exports = sumAll;

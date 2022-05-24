c8cfe4300190d79a23127d22a0207971c7f7aa82
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-24 16:08:07 -0500

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+const regex = /[A-Z]/gi;
+const forwards = text.toLowerCase().match(regex).join("");
+const backwards = text.toLowerCase().match(regex).reverse().join("");
+return forwards === backwards;    
 };
-
 // Do not edit below this line
 module.exports = palindromes;

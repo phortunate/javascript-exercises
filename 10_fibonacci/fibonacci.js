84a642e769b20b6687d5a908601d24da0cfebe29
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-25 11:30:04 -0500

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for ( let i = 1; i < number; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
+for (let i = 0; i < 100; i++) {
+    console.log(fibonacci(i));
+}
 // Do not edit below this line
 module.exports = fibonacci;

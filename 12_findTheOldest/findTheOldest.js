36a89d87547bdd315f10960cbeb251927b544411
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-25 14:17:38 -0500

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    const result = people.reduce((obj, person) => {
+        if (!person.yearOfDeath)  person.yearOfDeath = currentYear; 
+        if (Object.keys(obj).length === 0) obj = person;
+        const personAge = person.yearOfDeath - person.yearOfBirth;
+        const objAge = obj.yearOfDeath - obj.yearOfBirth;
+        return (personAge > objAge) ? person : obj;  
 
+    }, {});
+
+    return result;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
